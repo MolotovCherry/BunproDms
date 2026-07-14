f8e1f9524c27dffbf2a0c1eb801f5de946cc2838
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-07-13 18:42:44 -0700

    Custom parse date overflows from api

--- bunpro_api/src/objects.rs
+++ bunpro_api/src/objects.rs
@@ -1,10 +1,11 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, sync::Mutex};
 
 use jiff::{
-    Timestamp,
+    Span, Timestamp, ToSpan,
     civil::{Date, DateTime},
     tz::TimeZone,
 };
+use log::trace;
 use serde::{Deserialize, Deserializer, Serialize};
 
 pub type CardCount = u32;
@@ -27,10 +28,23 @@ pub struct ForecastHourly {
     pub vocab: ForecastHourlyObject,
 }
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize)]
 pub struct ForecastHourlyObject {
     #[serde(flatten)]
     pub rest: BTreeMap<Zoned, CardCount>,
+}
+
+impl<'de> Deserialize<'de> for ForecastHourlyObject {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // reset date on current so dates from next field don't carry over
+        *CURRENT.lock().map_err(serde::de::Error::custom)? = DateTime::ZERO;
+
+        let rest = BTreeMap::<Zoned, CardCount>::deserialize(deserializer)?;
+        Ok(ForecastHourlyObject { rest })
+    }
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
@@ -45,12 +59,19 @@ pub struct ForecastDailyObject {
 #[serde(transparent)]
 pub struct Zoned(#[serde(deserialize_with = "timestamp_to_zoned")] pub jiff::Zoned);
 
+static CURRENT: Mutex<DateTime> = Mutex::new(DateTime::ZERO);
+
 fn timestamp_to_zoned<'de, D>(de: D) -> Result<jiff::Zoned, D::Error>
 where
     D: Deserializer<'de>,
 {
     use std::sync::LazyLock;
+
     static CURRENT_TZ: LazyLock<TimeZone> = LazyLock::new(TimeZone::system);
+    static CURRENT_OFFSET: LazyLock<Span> = LazyLock::new(|| {
+        let offset = CURRENT_TZ.to_offset(Timestamp::now());
+        ((offset.seconds() / 3600) as i8).hours()
+    });
 
     let s = <&str as Deserialize>::deserialize(de)?;
 
@@ -59,12 +80,28 @@ where
         .parse()
         .map_err(serde::de::Error::custom)?;
 
-    let now = jiff::Zoned::new(Timestamp::now(), TimeZone::UTC)
+    let new_time = naive_dt.time() + *CURRENT_OFFSET;
+
+    let mut current = CURRENT.lock().unwrap();
+    if *current == DateTime::ZERO {
+        *current = naive_dt;
+    } else if new_time.hour() == 0 {
+        // we overflowed into the next day
+        *current += 1.day();
+    }
+
+    let date = naive_dt
         .with()
-        .time(naive_dt.time())
+        .date(current.date())
+        .time(new_time)
         .build()
         .map_err(serde::de::Error::custom)?
-        .with_time_zone(CURRENT_TZ.clone());
+        .to_zoned(CURRENT_TZ.clone())
+        .map_err(serde::de::Error::custom)?;
 
-    Ok(now)
+    *current = date.datetime();
+
+    trace!(hour:% = s, date:?; "parsing hour");
+
+    Ok(date)
 }
